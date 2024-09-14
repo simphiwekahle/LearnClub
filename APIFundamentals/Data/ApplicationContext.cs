a9ad8c7246426003c131a3aecc8a80746ab5d0f8
simphiwekahle <sim.kahle7@gmail.com>
2024-09-15 00:40:09 +0200

    API
    1. API Clean-up
    - Removing unneeded files
    - Add 'ApplicationContext.cs'
    * Add static data

---
+++ APIFundamentals/Data/ApplicationContext.cs
@@ -0,0 +1,56 @@
+﻿using Fundamentals.Models;
+
+namespace APIFundamentals.Data;
+
+public class ApplicationContext
+{
+	public List<AuthorModel> Authors =>
+	[
+		new()
+		{
+			Id = 1,
+			FirstName = "Gege",
+			LastName = "Akutami",
+			Books =
+			[
+				new()
+				{
+					Id = 1,
+					Title = "Jujutsu Kaisen",
+					Description = "Shibuya Incident",
+					ISBN = "4321"
+				}
+			]
+		},
+		new()
+		{
+			Id = 2,
+			FirstName = "Masashi",
+			LastName = "Kishimoto",
+			Books =
+			[
+				new()
+				{
+					Id = 2,
+					Title = "Naruto",
+					Description = "Chunin Exam",
+					ISBN = "3214"
+				},
+				new()
+				{
+					Id = 3,
+					Title = "Naruto Shipuden",
+					Description = "The 4th Great Ninja War",
+					ISBN = "2143"
+				},
+				new()
+				{
+					Id = 4,
+					Title = "Boruto",
+					Description = "Naruto - Next Generation",
+					ISBN = "1234"
+				}
+			]
+		},
+	];
+}
